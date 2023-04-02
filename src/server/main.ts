4793b21eebd592d75ea5dec13e80b12916c75ac0
Aqtoni <109670071+Aqtoni@users.noreply.github.com>
2023-04-02 12:28:34 +0300

    v5

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+  const API_PORT = 3003;
+  await app.listen(API_PORT);
 
+logger.log(`App listening on ${API_PORT}`);
   // Gracefully shutdown the server.
   app.enableShutdownHooks();
 }
